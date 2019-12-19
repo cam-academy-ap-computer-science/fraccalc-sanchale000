c75e78ac9d431dcfe8bf017d0556fe993abce09b
asped <asped@10.0.0.200>
2019-12-18 19:25:00 -0800

    Nearly Done?

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -44,6 +44,16 @@ public class FracCalc {
 	    	String numerTwo = "";
 	    	String denomTwo = "";
 	    	String wholeTwo = "";
+	    	String temporary = "";
+	    	int wholenumOne = 0;
+	    	int wholenumTwo = 0;
+	    	int numernumOne = 0;
+	    	int numernumTwo = 0;
+	    	int denomnumOne = 0;
+	    	int denomnumTwo = 0;
+	    	int tempInt = 0;
+	    	String tempString = "";
+	    	
 	    	
 	    	if (input.indexOf(" ") != -1) {
 	    		spaceLoc = input.indexOf(" ");
@@ -51,54 +61,86 @@ public class FracCalc {
 	    		numOne = input.substring(0, spaceLoc);
 	    		numTwo = input.substring(spaceLoc + 3, input.length());
 	    	} else {
-	    		System.out.println("Invalid Input");
-	    		
+	    		return "Error: invalid input";
 	    	}
+	    	System.out.println(numOne);
+	    	System.out.println(numTwo);
+	    	System.out.println(operator);
 	    	if (numOne.indexOf("_") != -1) { //whole number one
-	    		wholeOne = numOne.substring(0, input.indexOf("_"));	    		
+	    		wholeOne = numOne.substring(0, numOne.indexOf("_"));	    		
 	    	} else if (numOne.indexOf("/") == -1) { 
-	    		wholeOne = numOne.substring(0, spaceLoc);
+	    		wholeOne = numOne.substring(0, numOne.length());
 	    	} else { 
 	    		wholeOne = "0";
 	    	}
+	    	System.out.println("wholeOne " + wholeOne);
 	    	if (numTwo.indexOf("_") != -1) { //whole number two
-	    		wholeTwo = numTwo.substring(0, input.indexOf("_"));	    		
+	    		wholeTwo = numTwo.substring(0, numTwo.indexOf("_"));	    		
 	    	} else if (numTwo.indexOf("/") == -1) { 
-	    		wholeTwo = numTwo.substring(0, spaceLoc);
+	    		wholeTwo = numTwo.substring(0, numTwo.length());
 	    	} else { 
 	    		wholeTwo = "0";
 	    	}
-	    	if (numOne.indexOf("/") != -1) { //numerator one
+	    	System.out.println("wholeTwo " + wholeTwo);
+	    	if (numOne.indexOf("/") != -1) { //numerator two
 	    		if (numOne.indexOf("_") != -1) {
 	    			numerOne = numOne.substring(numOne.indexOf("_")+1, numOne.indexOf("/"));
 	    			denomOne = numOne.substring(numOne.indexOf("/")+1,numOne.length());
 	    		} else {
 	    			numerOne = numOne.substring(0, numOne.indexOf("/"));
+	    			denomOne = numOne.substring(numOne.indexOf("/")+1,numOne.length());
 	    		}
 	    	} else {
 	    		numerOne = "0";
+	    		denomOne = "1";
 	    	}
-	    	
-    	    if (numTwo.indexOf("/") != -1) { //numerator one
+	    	System.out.println("numerOne " + numerOne);
+	    	System.out.println("denomOne " + denomOne);
+    	    if (numTwo.indexOf("/") != -1) { //numerator two
     		    if (numTwo.indexOf("_") != -1) {
     			    numerTwo = numTwo.substring(numTwo.indexOf("_")+1, numTwo.indexOf("/"));
     			    denomTwo = numTwo.substring(numTwo.indexOf("/")+1, numTwo.length());
     		    } else {
     			    numerTwo = numTwo.substring(0, numTwo.indexOf("/"));
+    			    denomTwo = numTwo.substring(numTwo.indexOf("/")+1,numTwo.length());
     		    }
     	    } else {
     		    numerTwo = "0";
+    		    denomTwo = "1";
     	    }
+    	    System.out.println("numerTwo " + numerTwo);
+    	    System.out.println("denomTwo " + denomTwo);
     	    
-	    	
-	    	
-	    		
-	    	
-	    	
-	    	
-	        
-	        return numerTwo;
+    	    wholenumOne = Integer.parseInt(wholeOne); //parsing numbers
+    	    numernumOne = Integer.parseInt(numerOne);
+    	    denomnumOne = Integer.parseInt(denomOne);
+    	    wholenumTwo = Integer.parseInt(wholeTwo);
+    	    numernumTwo = Integer.parseInt(numerTwo);
+    	    denomnumTwo = Integer.parseInt(denomTwo);
+    	    numernumOne = (((wholenumOne * denomnumOne) + numernumOne) * denomnumTwo);
+    	    numernumTwo = (((wholenumTwo * denomnumTwo) + numernumTwo) * denomnumOne );
+    	    denomnumOne = (denomnumOne * denomnumTwo);
+    	    
+    	    if ((denomnumOne == 0) || (denomnumTwo == 0)) { //calculating values based on operator
+    	    	return "Error: divide by 0";
+    	    } else if (operator == '+') {
+    	    	numernumOne = numernumOne + numernumTwo;
+    	    } else if (operator == '-') {
+    	    	numernumOne = numernumOne + (-1 * numernumTwo);
+    	    } else if (operator == '*') {
+    	    	numernumOne = numernumOne * numernumTwo;
+    	    	denomnumOne = denomnumOne * denomnumOne;
+    	    } else if (operator == '/') {
+    	    	numernumOne = numernumOne * denomnumOne;
+    	    	denomnumOne = denomnumOne * numernumTwo;
+    	    } else {
+    	    	return "Error: invalid operator";
+    	    }
+    	    tempString = (numernumOne + "/" + denomnumOne);
+    	    
+	        return tempString;
 	    }
+	    
 
 	  
 	    
